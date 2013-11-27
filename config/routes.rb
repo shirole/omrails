f7dca063048a5a237730ae18f1baf41921ab3579
Sameer Shirole <shirole@hotmail.com>
2013-11-26 20:37:07 -0600

    Pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Omrails::Application.routes.draw do
+  root :to => 'pages#home'
+  get 'about' => 'pages#about'
+  get "pages/home"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
