8f904036f9a4b0f1b8319e0c730888bc25adec01
Sameer Shirole <shirole@hotmail.com>
2013-12-14 15:35:22 -0600

    Add user show method

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Omrails::Application.routes.draw do
+  get "users/show"
+
   resources :pins
 
   devise_for :users
+  match 'users/:id' => 'users#show', as: :user  
 
   root :to => 'pins#index'
   get 'about' => 'pages#about'
